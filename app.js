8a0405f60cd12bf25da122316e7989f673adfede
myoung8223 <141894282+myoung8223@users.noreply.github.com>
2026-06-07 15:57:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ---- BUILD VERSION CONTROLLER ----
-const BUILD_NUMBER = "216"; // <-- Incremented for SVG Import Database & Grid Layout
+const BUILD_NUMBER = "217"; // <-- Incremented for SVG Import Database & Grid Layout
 
 // 🍯 Import standalone, offline-ready CodeJar framework
 import { CodeJar } from './libs/codejar.min.js';
@@ -2867,7 +2867,7 @@ function isolateOpenSCADGhosts(code, stripAllGhostsMode = false) {
         return {
             solidContent: fullSolidBlock,
             content:      fullSolidBlock,
-            ghostContent: ghostSpill,
+            ghostContent: ghostSpill ? `${expression}\n{\n${ghostSpill}}\n` : "",
             containsGhost: false, hasNestedGhost: false, isSelfGhost: false
         };
     }
